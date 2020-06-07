db05d86cae5f3927a2d256b96a64911263f65491
phibzy <phibzy@gmail.com>
2020-06-07 15:23:43 +1000

    Will debug problem 3 later, day off today

--- Leetcode/Jun20/browserHistory.py
+++ Leetcode/Jun20/browserHistory.py
@@ -91,3 +91,14 @@ class BrowserHistory:
 # obj.visit(url)
 # param_2 = obj.back(steps)
 # param_3 = obj.forward(steps)
+# BrowserHistory browserHistory = new BrowserHistory("leetcode.com");
+# browserHistory.visit("google.com");       // You are in "leetcode.com". Visit "google.com"
+# browserHistory.visit("facebook.com");     // You are in "google.com". Visit "facebook.com"
+# browserHistory.visit("youtube.com");      // You are in "facebook.com". Visit "youtube.com"
+# browserHistory.back(1);                   // You are in "youtube.com", move back to "facebook.com" return "facebook.com"
+# browserHistory.back(1);                   // You are in "facebook.com", move back to "google.com" return "google.com"
+# browserHistory.forward(1);                // You are in "google.com", move forward to "facebook.com" return "facebook.com"
+# browserHistory.visit("linkedin.com");     // You are in "facebook.com". Visit "linkedin.com"
+# browserHistory.forward(2);                // You are in "linkedin.com", you cannot move forward any steps.
+# browserHistory.back(2);                   // You are in "linkedin.com", move back two steps to "facebook.com" then to "google.com". return "google.com"
+# browserHistory.back(7);                   // You are in "google.com", you can move back only one step to "leetcode.com". return "leetcode.com"
