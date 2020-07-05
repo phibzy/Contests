22371b94b6125f78e4e5b7385f3075a1fa3afceb
phibzy <phibzy@gmail.com>
2020-07-05 14:07:06 +1000

    1 out of 4

---
+++ Leetcode/Jul20/050720/AntPlank/antPlank.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+  @author      : Chris Phibbs
+  @created     : Sunday Jul 05, 2020 12:45:53 AEST
+  @file        : antPlank
+
+"""
+
+class Solution:
+    def getLastMoment(self, n, left, right):
+        realLeft = [0]*(n+1)
+        for i in left:
+            realLeft[i] = 1
+
+        realRight = [0]*(n+1)
+        for i in right:
+            realRight[i] = 1
+
+        """
+        Base case: left array is at position n, delete
+                   right array is at position 0, delete
+
+        General case:
+                First pass: if ant in front of you is facing you, turn around
+                            Ants that turn around don't make another move
+                Second pass: if ant is in same place as you, turn around
+                update lists accordingly
+
+        """
+
+
+
+        """
+        The ants literally walk through each other
+        Don't keep track of who's who
+
+        """
+        
+
+
+
+
+
---
+++ Leetcode/Jul20/050720/AntPlank/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+   Test Cases:
+
+
+
+"""
+
+import unittest
+from antPlank import Solution
+
+class testPlank(unittest.TestCase):
+
+    a = Solution()
+
+    def testAllRight(self):
+        self.assertEqual(self.a.getLastMoment(7, [], [0,1,2,3,4,5,6,7]), 7)
+
+    def testAllLeft(self):
+        self.assertEqual(self.a.getLastMoment(7, [0,1,2,3,4,5,6,7], []), 7)
